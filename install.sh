f5e7fa70e4dfa1469a47e28f251ec0c1830ca52f
Francisco Espino <hola@fespino.es>
2017-10-02 02:07:54 +0100

    initial commit

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+command_exists() {
+    type "$1" > /dev/null 2>&1
+}
+
+echo "Installing dotfiles."
+
+source install/link.sh
+
+echo "creating vim directories"
+mkdir -p ~/.vim-tmp
+
+if ! command_exists zsh; then
+    echo "zsh not found. Please install and then re-run installation script"
+    exit 1
+elif ! [[ $SHELL =~ .*zsh.* ]]; then
+    echo "Configuring zsh as default shell"
+    chsh -s $(which zsh)
+fi
+
+echo "Done. Reload your terminal."
